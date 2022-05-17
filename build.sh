0ac3ea6a9d5f1ed3a3342ad6fc47a625fa6612d5
yangzhinong <66845701@qq.com>
2022-05-17 12:50:47 +0800

    Update build.sh
    change to 192.168.9.1

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ cd ${OP_BUILD_PATH}/immortalwrt
 ./scripts/feeds update -a && ./scripts/feeds install -a
 rm -rf ./tmp && rm -rf .config
 mv ${OP_BUILD_PATH}/ssr.config ${OP_BUILD_PATH}/immortalwrt/.config
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generate
 make defconfig
 make download -j8
 make V=s -j$(nproc)
